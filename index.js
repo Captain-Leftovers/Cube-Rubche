6ddb109765588da3b0486e23439486918850af18
Dobromir Stefanov Ivanov <61572237+Captain-Leftovers@users.noreply.github.com>
2021-10-07 17:10:49 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('started');
+const express = require("express");
+
+const app = express();
+
+app.all("/", (req, res) => {
+  res.write("it's working");
+  res.end();
+});
+
+app.listen(5000, ()=>{
+    console.log('Application is running on http://localhost:5000');
+})
