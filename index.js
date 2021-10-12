d21a1d54df8b387e9f4d805aea80c7b82bd2b493
Dobromir Stefanov Ivanov <61572237+Captain-Leftovers@users.noreply.github.com>
2021-10-12 15:51:11 +1100

    added config.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const initHandlebars = require("./config/handlebars");
+const config = require('./config/config.json')[process.env.NODE_ENV || 'development'] 
 const app = express();
 initHandlebars(app);
 app.use(express.urlencoded({ extended: true }));
@@ -8,6 +9,6 @@ const routes = require("./config/routes");
 app.use(express.static("static"));
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log("Application is running on http://localhost:3000");
+app.listen(config.PORT, () => {
+  console.log(`Application is running on http://localhost:${config.PORT}`);
 });
