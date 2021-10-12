5663676c4304e9347e5bceb1ef0c99b7ce829a06
Dobromir Stefanov Ivanov <61572237+Captain-Leftovers@users.noreply.github.com>
2021-10-12 18:24:27 +1100

    added search

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,22 @@
-const express = require('express');
-const cubeService = require('../services/cubeService');
+const express = require("express");
+const cubeService = require("../services/cubeService");
 const router = express.Router();
-router.get("/", (req, res) => {
-let cubes = cubeService.getAll();
-    res.render('home', /* {layout: "test"} */{
-      cubes
-    })
-  });
 
-  module.exports = router;
+const searchFn = (req, res) => {
+  let { search, from, to } = req.query;
+  let cubes = cubeService.search(search, from, to);
+
+  res.render("home", { cubes, search, from, to, title: 'SEARCH'});
+};
+router.get("/search", searchFn);
+router.get("/", (req, res) => {
+  let cubes = cubeService.getAll();
+  res.render(
+    "home",
+    /* {layout: "test"} */ {
+      cubes,
+    }
+  );
+});
+
+module.exports = router;
--- services/cubeService.js
+++ services/cubeService.js
@@ -1,6 +1,5 @@
 const Cube = require("../models/Cube");
 const create = (name, description, imageUrl, difficulty) => {
-
   let cube = new Cube(name, description, imageUrl, difficulty);
   Cube.addCube(cube);
 };
@@ -9,6 +8,19 @@ const getAll = () => {
   return Cube.cubes;
 };
 
+const search = (search, from, to)=>{
+  let result = Cube.cubes.slice();
+  if(search){
+    result = result.filter(el => el.name.toLowerCase().includes(search.toLowerCase()));
+  }
+  if(from){
+    result = result.filter(el=> Number(el.difficulty) >= Number(from))
+  } 
+  if(to){
+    result = result.filter(el=>Number(el.difficulty) <= Number(to))
+  }
+  return result
+}
 const getOne = (id) => {
   return Cube.cubes.filter((el) => el.id == id)[0];
 };
@@ -16,5 +28,6 @@ const cubeService = {
   create,
   getAll,
   getOne,
+  search
 };
 module.exports = cubeService;
