5044a4b9feaf183add5d41c309b5e2a2ad0a3ed8
Dobromir Stefanov Ivanov <61572237+Captain-Leftovers@users.noreply.github.com>
2021-10-15 07:59:03 +1100

    added service update

--- services/cubeService.js
+++ services/cubeService.js
@@ -1,33 +1,40 @@
 const Cube = require("../models/Cube");
 const create = (name, description, imageUrl, difficulty) => {
-  let cube = new Cube(name, description, imageUrl, difficulty);
-  Cube.addCube(cube);
+  let cube = new Cube({
+    name,
+    description,
+    imageUrl,
+    difficulty,
+  });
+  return cube.save();
 };
 
 const getAll = () => {
-  return Cube.cubes;
+  return Cube.find({});
 };
 
-const search = (search, from, to)=>{
+const search = (search, from, to) => {
   let result = Cube.cubes.slice();
-  if(search){
-    result = result.filter(el => el.name.toLowerCase().includes(search.toLowerCase()));
+  if (search) {
+    result = result.filter((el) =>
+      el.name.toLowerCase().includes(search.toLowerCase())
+    );
   }
-  if(from){
-    result = result.filter(el=> Number(el.difficulty) >= Number(from))
-  } 
-  if(to){
-    result = result.filter(el=>Number(el.difficulty) <= Number(to))
+  if (from) {
+    result = result.filter((el) => Number(el.difficulty) >= Number(from));
   }
-  return result
-}
+  if (to) {
+    result = result.filter((el) => Number(el.difficulty) <= Number(to));
+  }
+  return result;
+};
 const getOne = (id) => {
-  return Cube.cubes.filter((el) => el.id == id)[0];
+  return Cube.findById(id);
 };
 const cubeService = {
   create,
   getAll,
   getOne,
-  search
+  search,
 };
 module.exports = cubeService;
