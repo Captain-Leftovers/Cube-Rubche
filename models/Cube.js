77595feb71c6e40b84883a02d4b436b5a08bc817
Dobromir Stefanov Ivanov <61572237+Captain-Leftovers@users.noreply.github.com>
2021-10-14 18:06:10 +1100

    added cube model

--- models/Cube.js
+++ models/Cube.js
@@ -1,42 +1,77 @@
-const uniqid = require('uniqid');
+const mongoose = require("mongoose");
 
-class Cube {
-  static #cubesList = [
-    {
-      name: "photo cube",
-      description: " good photo of a cube",
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUfTOsK3KOfszlni0bk7r781Hl-PrbEimGKA&usqp=CAU",
-      difficulty: "6",
-      id: '1sd122f122dd',
-    },
-    {
-    name: "simple cube",
-    description: "just another simple cube :)",
-    imageUrl:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQChxO2Se7TnxPexFRwNCb9EDB-3JjzfceLRw&usqp=CAU',
-    difficulty: "2",
-    id: 'g1g1g1y819euj',
-
+const cubeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  
-  ];
-  constructor(name, description, imageUrl, difficulty) {
-    this.name = name;
-    this.description = description;
-    this.imageUrl = imageUrl;
-    this.difficulty = difficulty;
-    this.id = uniqid();
-  }
-  static get cubes(){
-    return Cube.#cubesList.slice();
-  }
+  description: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    // validate: /^https?:\/\//i
+    validate: {
+      function(value) {
+        return /^https?:\/\//i;
+      },
+      message: "image Url is invalid",
+    },
+  },
+  difficulty: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+});
 
-  static addCube(cube) {
-    Cube.#cubesList.push(cube);
-
-  } 
-
-}
-
+// cubeSchema.path('imageUrl').validate = function (value) {
+//   return /^https?:\/\//i
+// }
+const Cube = mongoose.model("Cube", cubeSchema);
 module.exports = Cube;
+
+// class Cube {
+//   static #cubesList = [
+//     {
+//       name: "photo cube",
+//       description: " good photo of a cube",
+//       imageUrl:
+//         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUfTOsK3KOfszlni0bk7r781Hl-PrbEimGKA&usqp=CAU",
+//       difficulty: "6",
+//       id: '1sd122f122dd',
+//     },
+//     {
+//     name: "simple cube",
+//     description: "just another simple cube :)",
+//     imageUrl:
+//       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQChxO2Se7TnxPexFRwNCb9EDB-3JjzfceLRw&usqp=CAU',
+//     difficulty: "2",
+//     id: 'g1g1g1y819euj',
+
+//   },
+
+//   ];
+//   constructor(name, description, imageUrl, difficulty) {
+//     this.name = name;
+//     this.description = description;
+//     this.imageUrl = imageUrl;
+//     this.difficulty = difficulty;
+//     this.id = uniqid();
+//   }
+//   static get cubes(){
+//     return Cube.#cubesList.slice();
+//   }
+
+//   static addCube(cube) {
+//     Cube.#cubesList.push(cube);
+
+//   }
+
+// }
+
+// module.exports = Cube;
