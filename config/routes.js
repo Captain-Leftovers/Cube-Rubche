2725617ecb167daa4c5dfced9bf7b8cbc4182ace
Dobromir Stefanov Ivanov <61572237+Captain-Leftovers@users.noreply.github.com>
2021-10-17 19:40:37 +1100

    added create accessory

--- config/routes.js
+++ config/routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const homeController = require("../controllers/homeController");
 const cubeController = require("../controllers/cubeController");
 const aboutController = require("../controllers/aboutController");
-
+const accessoryController = require('../controllers/accessoryController');
 const router = express.Router();
 
 router.use(homeController);
 router.use("/cube", cubeController);
+router.use("/accessory", accessoryController);
 router.use(aboutController);
 router.all("*", (req, res) => {
   res.render("404");
