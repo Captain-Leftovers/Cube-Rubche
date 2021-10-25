a929fa975da6bbf244e92244f709de03dacabb63
Dobromir Stefanov Ivanov <61572237+Captain-Leftovers@users.noreply.github.com>
2021-10-26 10:33:04 +1100

    added login hbs and sadface to 404 page

--- config/routes.js
+++ config/routes.js
@@ -2,13 +2,15 @@ const express = require("express");
 const homeController = require("../controllers/homeController");
 const cubeController = require("../controllers/cubeController");
 const aboutController = require("../controllers/aboutController");
-const accessoryController = require('../controllers/accessoryController');
+const accessoryController = require("../controllers/accessoryController");
+const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.use(homeController);
 router.use("/cube", cubeController);
 router.use("/accessory", accessoryController);
 router.use(aboutController);
+router.use("/auth", authController);
 router.all("*", (req, res) => {
   res.render("404");
 });
